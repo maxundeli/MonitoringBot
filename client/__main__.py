abb567fa283a482d7d6e64312774038ec7f9ce0e
maxun <maxundely@gmail.com>
2025-06-09 18:00:37 +0500

    )3

--- client/__main__.py
+++ client/__main__.py
@@ -21,7 +21,7 @@ import shutil
 import subprocess
 
 import psutil
-from .worker import run_speedtest, run_diagnostics, submit
+from client.worker import run_speedtest, run_diagnostics, submit
 # Кэш для вычисления CPU без задержки
 PROC_CACHE: dict[int, tuple[float, float]] = {}
 GPU_VENDOR: str | None = None
